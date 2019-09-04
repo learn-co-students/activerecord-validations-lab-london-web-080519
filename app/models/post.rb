01d9d78fe7ef8877d9a691053bfe62b687297165
oliburt <oliburt@gmail.com>
2019-09-04 18:28:37 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :clickbait_validator
+
+    def clickbait?
+        result = false
+        indicators = [/Won't Believe/, /Secret/, /Top \d\d?/, /Guess/]
+        indicators.each do |indicator|
+            if self.title.match(indicator)
+                result = true
+            end
+        end
+        result
+    end
+
+    def clickbait_validator
+        if self.title && !self.clickbait?
+            errors.add(:title, 'is not clickbaity enough')
+        end
+    end
+    
 end
