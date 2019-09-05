ade74acb89a2a11fb7ef66b00eb878691a6ecb86
tegansims <github email address>
2019-09-05 13:59:15 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: :true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Ficion"]} 
+    validate :is_clickbait?
+
+
+    def is_clickbait?
+        clickbaits = ["Won't Believe", "Secret", "Top [number]", "Guess" ]
+        if title != nil && clickbaits.none?{|clickbait| self.title.include?(clickbait)}
+            errors.add(:is_clickbait?, 'Invalid title')
+        end
+    end
 end
