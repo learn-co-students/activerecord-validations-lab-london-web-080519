6eb1754c13e485a7b8dd14d648bdbf762b888af1
99chris99 <chrishodge87@gmail.com>
2019-09-05 16:01:59 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Post.create(title: "bbb cccc", content: "contente here", summary: "summary", category: "category")
