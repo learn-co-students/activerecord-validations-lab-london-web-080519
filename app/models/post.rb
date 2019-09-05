80fb56ec7ac87b1c5e91bd4489c5b8f8d39fe188
PollyCR <polly.c.reynolds@gmail.com>
2019-09-05 14:50:51 +0100

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,40 @@
+require 'pry'
 class Post < ActiveRecord::Base
+    belongs_to :author 
+    validates :title, presence: true 
+    validates :summary, length: { maximum: 250 }
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+
+    def clickbait?
+        result = false
+        indicators = [/Won't Believe/, /Secret/, /Top \d\d?/, /Guess/]
+        indicators.each do |indicator|
+            if self.title.match(indicator)
+                result = true
+            end
+        end
+        result
+    end
+
+    def is_clickbait?
+        if self.title && !self.clickbait?
+            errors.add(:title, 'is not clickbaity enough')
+        end
+    end
+
+
+
+
+
+            #search through the list of titles
+
+            #find titles matching any of the given strings 
+
+            #find article associated with that title 
+
+            #return article 
+
+       
 end
